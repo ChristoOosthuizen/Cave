537c276eaf706bc134a659b312739c5b0bae9c86
TheoryGames <shane@theorygames.ca>
2015-05-03 22:18:15 -0700

    More triggers

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Eyes : MonoBehaviour {
+
+    public GameObject eyes;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        eyes.SetActiveRecursively(false);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        eyes.SetActiveRecursively(true);
+    }
+}
