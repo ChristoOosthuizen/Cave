3ec85feb1c332d52139e5ce2c35327067f4949ee
Christo <christo.gordon.3d@gmail.com>
2015-05-08 01:15:49 +0200

    Update

---
+++ Assets/Game Assets/Scripts/FlashLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour 
+{
+	private Light flashLight;
+	public bool lightOn = false;
+	
+	
+	void Start ()
+	{
+		flashLight = GetComponent<Light>();
+	}
+
+	void Update ()
+	{
+		if(lightOn == false)
+		{
+			if(Input.GetKeyDown(KeyCode.F))
+			{
+				flashLight.enabled = true;
+				lightOn = true;
+			}
+		}
+
+
+		if(Input.GetKeyUp(KeyCode.F)& lightOn == true)
+		{
+			flashLight.enabled = false;
+			lightOn = false;
+		}
+	}
+}
---
+++ Assets/Game Assets/Scripts/PlayerVoiceTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerVoiceTrigger : MonoBehaviour 
+{
+	public AudioClip impact;
+	AudioSource audio;
+	
+	void Start() 
+	{
+		audio = GetComponent<AudioSource>();
+	}
+	
+	void OnTriggerEnter() 
+	{
+		audio.PlayOneShot(impact, 0.7F);
+		gameObject.GetComponent<Collider>().enabled = false;
+
+	}
+
+}
